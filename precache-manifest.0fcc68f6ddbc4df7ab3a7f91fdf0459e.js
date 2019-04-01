dc57250a368c30573cca24dbc01be74b375d178d
Andy CW Wu <andy_cw_wu@wistron.com>
2019-04-01 09:21:31 +0800

    Updates

--- precache-manifest.0fcc68f6ddbc4df7ab3a7f91fdf0459e.js
+++ precache-manifest.0fcc68f6ddbc4df7ab3a7f91fdf0459e.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/wedevote-nogerm/static/media/outline-icons.701ae6ab.eot"
   },
   {
-    "revision": "826f9cc9cd76615a8169",
+    "revision": "3e975c891e896b8f27bd",
     "url": "/wedevote-nogerm/static/css/main.f5a1043e.chunk.css"
   },
   {
@@ -28,56 +28,56 @@ self.__precacheManifest = [
     "url": "/wedevote-nogerm/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/wedevote-nogerm/static/media/icons.b87b9ba5.ttf"
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/wedevote-nogerm/static/media/icons.0ab54153.woff2"
   },
   {
     "revision": "faff92145777a3cbaf8e7367b4807987",
     "url": "/wedevote-nogerm/static/media/icons.faff9214.woff"
   },
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/wedevote-nogerm/static/media/outline-icons.cd6c777f.woff2"
-  },
-  {
-    "revision": "0ab54153eeeca0ce03978cc463b257f7",
-    "url": "/wedevote-nogerm/static/media/icons.0ab54153.woff2"
-  },
-  {
-    "revision": "826f9cc9cd76615a8169",
-    "url": "/wedevote-nogerm/static/js/main.f45c44c2.chunk.js"
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/wedevote-nogerm/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "962a1bf31c081691065fe333d9fa8105",
     "url": "/wedevote-nogerm/static/media/icons.962a1bf3.svg"
   },
   {
+    "revision": "3e975c891e896b8f27bd",
+    "url": "/wedevote-nogerm/static/js/main.f89d976b.chunk.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/wedevote-nogerm/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/wedevote-nogerm/static/media/outline-icons.ef60a4f6.woff"
-  },
-  {
-    "revision": "e8c322de9658cbeb8a774b6624167c2c",
-    "url": "/wedevote-nogerm/static/media/brand-icons.e8c322de.woff2"
-  },
-  {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/wedevote-nogerm/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
     "url": "/wedevote-nogerm/static/media/outline-icons.ad97afd3.ttf"
   },
   {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/wedevote-nogerm/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/wedevote-nogerm/static/media/brand-icons.13db00b7.eot"
   },
   {
-    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
-    "url": "/wedevote-nogerm/static/media/brand-icons.c5ebe0b3.ttf"
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/wedevote-nogerm/static/media/brand-icons.e8c322de.woff2"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
     "url": "/wedevote-nogerm/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/wedevote-nogerm/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
     "revision": "a1a749e89f578a49306ec2b055c073da",
@@ -88,7 +88,7 @@ self.__precacheManifest = [
     "url": "/wedevote-nogerm/static/css/2.677f6d08.chunk.css"
   },
   {
-    "revision": "bed3974935fc88c0675c5bff2232e9d1",
+    "revision": "e22fb81d01aecef307c2856f2c0f5636",
     "url": "/wedevote-nogerm/index.html"
   }
 ];
