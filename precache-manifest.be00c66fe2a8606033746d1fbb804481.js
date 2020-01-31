97e31a233385f59c7a92fce1037c749c8d92e31c
Andy CW Wu <andy_cw_wu@wistron.com>
2020-01-31 12:46:29 +0800

    Updates

--- precache-manifest.be00c66fe2a8606033746d1fbb804481.js
+++ precache-manifest.be00c66fe2a8606033746d1fbb804481.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "2a3136e5c47527a389c5",
+    "revision": "05a3468e7e52f68cc431",
     "url": "/wedevote-nogerm/static/css/main.0157cb6a.chunk.css"
   },
   {
-    "revision": "2a3136e5c47527a389c5",
-    "url": "/wedevote-nogerm/static/js/main.913e43dc.chunk.js"
+    "revision": "05a3468e7e52f68cc431",
+    "url": "/wedevote-nogerm/static/js/main.8fa08796.chunk.js"
   },
   {
     "revision": "972e68c25224a6179bd0",
     "url": "/wedevote-nogerm/static/js/runtime~main.584d38f4.js"
   },
   {
-    "revision": "daf85e4a2c097c8a00ba",
+    "revision": "ce6741b8fde9c87b67a4",
     "url": "/wedevote-nogerm/static/css/2.7c386e54.chunk.css"
   },
   {
-    "revision": "daf85e4a2c097c8a00ba",
-    "url": "/wedevote-nogerm/static/js/2.be5976cf.chunk.js"
+    "revision": "ce6741b8fde9c87b67a4",
+    "url": "/wedevote-nogerm/static/js/2.e359cb67.chunk.js"
   },
   {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
@@ -32,20 +32,12 @@ self.__precacheManifest = [
     "url": "/wedevote-nogerm/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/wedevote-nogerm/static/media/flags.9c74e172.png"
-  },
-  {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/wedevote-nogerm/static/media/outline-icons.701ae6ab.eot"
-  },
-  {
     "revision": "cd6c777f1945164224dee082abaea03a",
     "url": "/wedevote-nogerm/static/media/outline-icons.cd6c777f.woff2"
   },
   {
-    "revision": "962a1bf31c081691065fe333d9fa8105",
-    "url": "/wedevote-nogerm/static/media/icons.962a1bf3.svg"
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/wedevote-nogerm/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
@@ -56,39 +48,47 @@ self.__precacheManifest = [
     "url": "/wedevote-nogerm/static/media/outline-icons.ad97afd3.ttf"
   },
   {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/wedevote-nogerm/static/media/icons.8e3c7f55.eot"
-  },
-  {
-    "revision": "a046592bac8f2fd96e994733faf3858c",
-    "url": "/wedevote-nogerm/static/media/brand-icons.a046592b.woff"
-  },
-  {
-    "revision": "e8c322de9658cbeb8a774b6624167c2c",
-    "url": "/wedevote-nogerm/static/media/brand-icons.e8c322de.woff2"
-  },
-  {
-    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
-    "url": "/wedevote-nogerm/static/media/brand-icons.c5ebe0b3.ttf"
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/wedevote-nogerm/static/media/icons.962a1bf3.svg"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
     "url": "/wedevote-nogerm/static/media/outline-icons.82f60bd0.svg"
   },
   {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/wedevote-nogerm/static/media/brand-icons.13db00b7.eot"
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/wedevote-nogerm/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/wedevote-nogerm/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/wedevote-nogerm/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/wedevote-nogerm/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/wedevote-nogerm/static/media/flags.9c74e172.png"
   },
   {
     "revision": "a1a749e89f578a49306ec2b055c073da",
     "url": "/wedevote-nogerm/static/media/brand-icons.a1a749e8.svg"
   },
   {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/wedevote-nogerm/static/media/brand-icons.13db00b7.eot"
+  },
+  {
     "revision": "60a89fa1248f47a49807e81a80797f64",
     "url": "/wedevote-nogerm/static/media/LINE@_APP_typeA.60a89fa1.png"
   },
   {
-    "revision": "a5790d6872b3a7efdc80358d5ba6f482",
+    "revision": "0a0b2c84edd9796ac8a6f8d1a9d559f3",
     "url": "/wedevote-nogerm/index.html"
   }
 ];
